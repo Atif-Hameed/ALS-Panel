66e7064d11a8efc27de64470a2f1016a53ca1fe6
Atif Hameed <atifhameed11312@gmail.com>
2025-05-07 16:59:18 +0500

    new updates

--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/agent-search");
-  return null;
+// export default function Home() {
+//   redirect("/agent-search");
+//   return null;
+// }
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      dkj
+    </div>
+  )
 }
+
+export default page
